02821ed152ab83861661ee0a39d1cfddd022de47
Bruno <devbrunopaula@gmail.com>
2021-11-16 11:28:13 -0500

    Adds a new Zipcode Model

--- app/Models/Franchise.ts
+++ app/Models/Franchise.ts
@@ -1,13 +1,24 @@
 import { DateTime } from 'luxon'
-import { BaseModel, column } from '@ioc:Adonis/Lucid/Orm'
+import { BaseModel, column, HasMany, hasMany } from '@ioc:Adonis/Lucid/Orm'
+import Zipcode from './Zipcode'
+import Customer from './Customer'
 
 export default class Franchise extends BaseModel {
   @column({ isPrimary: true })
   public id: number
+
+  @column()
+  public name: string
 
   @column.dateTime({ autoCreate: true })
   public createdAt: DateTime
 
   @column.dateTime({ autoCreate: true, autoUpdate: true })
   public updatedAt: DateTime
+
+  @hasMany(() => Zipcode)
+  public zipcodes: HasMany<typeof Zipcode>
+
+  @hasMany(() => Customer)
+  public customers: HasMany<typeof Customer>
 }
