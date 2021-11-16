e845d13fc903a4aff7f8c3760b0d64ef87f404f6
Bruno <devbrunopaula@gmail.com>
2021-11-16 11:27:21 -0500

    Add a new Franchise Controller

--- app/Controllers/Http/FranchisesController.ts
+++ app/Controllers/Http/FranchisesController.ts
@@ -1,13 +1,28 @@
 import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import Database from '@ioc:Adonis/Lucid/Database'
+import Franchise from 'App/Models/Franchise'
+import Zipcode from 'App/Models/Zipcode'
 
 export default class FranchisesController {
-  public async index({}: HttpContextContract) {}
+  public async index({ request, response, params }: HttpContextContract) {
+    const data = await Franchise.query().preload('zipcodes')
+
+    response.send(data)
+  }
 
   public async create({}: HttpContextContract) {}
 
   public async store({}: HttpContextContract) {}
 
-  public async show({}: HttpContextContract) {}
+  public async show({ response, params }: HttpContextContract) {
+    const [data] = await Zipcode.query().where('zipcode', params.id)
+
+    const zipcodeMatcher = data
+      ? { franchiseAvailable: true, zipcode: data.zipcode }
+      : { franchiseAvailable: false, zipcode: null }
+
+    response.status(200).json(zipcodeMatcher)
+  }
 
   public async edit({}: HttpContextContract) {}
 
