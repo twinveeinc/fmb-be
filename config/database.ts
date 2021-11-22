08464022a1072957ffbe4592696973f977cb3212
Bruno <devbrunopaula@gmail.com>
2021-11-22 16:03:22 -0500

    adds phone and email to owner model

--- config/database.ts
+++ config/database.ts
@@ -56,20 +56,4 @@ const databaseConfig: DatabaseConfig = {
   },
 }
 
-const pool = (() => {
-  if (process.env.NODE_ENV !== 'production') {
-    return new Pool({
-      connectionString: process.env.DATABASE_URL,
-      ssl: false,
-    })
-  } else {
-    return new Pool({
-      connectionString: process.env.DATABASE_URL,
-      ssl: {
-        rejectUnauthorized: false,
-      },
-    })
-  }
-})()
-
 export default databaseConfig
